f8d405b6807fcf4f2bf5a8299319f2f55b2f06b2
praveenece431 <97307468+praveenece431@users.noreply.github.com>
2023-11-05 20:05:41 +0530

    Update terraform-install-in-ubuntu.sh.sh

--- terraform-install-in-ubuntu.sh.sh
+++ terraform-install-in-ubuntu.sh.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt install unzip
 ## wget https://releases.hashicorp.com/terraform/<VERSION>/terraform_<VERSION>_linux_amd64.zip
 echo "*** Get releases from: https://releases.hashicorp.com/terraform/  *** "
-wget https://releases.hashicorp.com/terraform/1.6.0/terraform_1.6.0_linux_amd64.zi
+wget https://releases.hashicorp.com/terraform/1.6.0/terraform_1.6.0_linux_amd64.zip
 unzip terraform_1.6.0_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 terraform version
