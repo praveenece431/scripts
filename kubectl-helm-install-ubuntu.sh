8c37de08ba779286400a403d3940adda28c82893
praveenece431 <97307468+praveenece431@users.noreply.github.com>
2023-11-23 00:33:53 +0530

    Create kubectl-helm-install-ubuntu.sh

---
+++ kubectl-helm-install-ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y azure-cli
+sudo apt install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt install -y kubectl
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
+### HELM install ###
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
