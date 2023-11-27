71e4bfe75d00f61653ade0865e338d9ee6990715
praveenece431 <97307468+praveenece431@users.noreply.github.com>
2023-11-27 21:10:03 +0530

    Update pre-requisites.sh

--- pre-requisites.sh
+++ pre-requisites.sh
@@ -41,3 +41,26 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 }
 
+docker-install() {
+### Docker installation steps in Ubuntu ###
+sudo apt update
+# Install Prerequisites:
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+# Add Docker's official GPG key to verify the integrity of the packages.
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# Add the Docker stable repository to your system.
+echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Update the package list with the new Docker repository.
+sudo apt update
+# Install the Docker engine.
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+# Start docker service
+sudo systemctl start docker
+# Enable Docker to start on boot.
+sudo systemctl enable docker
+# Add Your User to the Docker Group (Optional, but recommended for using Docker without sudo):
+#sudo usermod -aG docker <your-username>
+sudo usermod -aG docker ubuntu
+# Verify docker version
+docker --version
+}
