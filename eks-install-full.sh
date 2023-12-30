b701dd2f7c6539be582b8fbf9581620266c4425a
praveenece431 <97307468+praveenece431@users.noreply.github.com>
2023-12-30 23:31:50 +0530

    Update eks-install-full.sh

--- eks-install-full.sh
+++ eks-install-full.sh
@@ -23,9 +23,9 @@ export AWS_ACCESS_KEY_ID="$keyId"
 export AWS_SECRET_ACCESS_KEY="$secretKey"
 export AWS_DEFAULT_REGION=us-east-1
 sudo apt  install docker.io -y
-sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.3/2023-11-14/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 kubectl version --short --client
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/bin
