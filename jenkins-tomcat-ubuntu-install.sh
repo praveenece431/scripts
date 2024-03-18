fba9860687e1a89916d88cab1023ec80b9f62326
PRAVEEN KUMAR <97307468+praveenece431@users.noreply.github.com>
2024-03-19 01:31:10 +0530

    Create jenkins-tomcat-ubuntu-install.sh

---
+++ jenkins-tomcat-ubuntu-install.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+echo "*** Installing JDK 11, Tomcat, Jenkins ***"
+sudo apt update
+sudo apt install openjdk-11-jdk
+wget https://get.jenkins.io/war-stable/latest/jenkins.war
+https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.87/bin/apache-tomcat-9.0.87.tar.gz
+tar -xvf apache-tomcat-9.0.87.tar.gz
+mv apache-tomcat-9.0.87 tomcat
+echo 'export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64' >> ~/.bashrc
+source ~/.bashrc
+sudo mv jenkins.war tomcat/webapps/
+./tomcat/bin/startup.sh
