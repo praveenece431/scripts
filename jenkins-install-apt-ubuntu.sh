67545a0be44a796eb547d235db41f2fa6597a854
PRAVEEN KUMAR <97307468+praveenece431@users.noreply.github.com>
2024-03-19 01:48:48 +0530

    Create jenkins-install-apt-ubuntu.sh

---
+++ jenkins-install-apt-ubuntu.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+echo "##### Install Jenkins with apt package manager ########"
+sudo apt update
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
