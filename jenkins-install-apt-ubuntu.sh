a5068a657c2fe9d8c2e9b79774f64000c13ce824
PRAVEEN KUMAR <97307468+praveenece431@users.noreply.github.com>
2024-04-24 17:24:30 +0530

    Update jenkins-install-apt-ubuntu.sh

--- jenkins-install-apt-ubuntu.sh
+++ jenkins-install-apt-ubuntu.sh
@@ -4,7 +4,10 @@ echo "##### Install Jenkins with apt package manager ########"
 sudo apt update
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
+sudo apt install openjdk-8-jre
+# set default version as Java 8 below
+sudo update-alternatives --config java
+java -version
 sudo apt install jenkins
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
